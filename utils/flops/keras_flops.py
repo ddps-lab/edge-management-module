459c96be3140348925ec49bc02c6eb7e971c7799
Jungae Park <46885199+jungae-park@users.noreply.github.com>
2022-08-13 18:42:32 +0900

    Update keras_flops.py
    code 정리

--- utils/flops/keras_flops.py
+++ utils/flops/keras_flops.py
@@ -11,5 +11,5 @@ mobilenet_v2 = mobilenet_v2.MobileNetV2(weights='imagenet')
 inception_v3 = inception_v3.InceptionV3(weights='imagenet')
 
 # Calculae FLOPS
-flops = get_flops(yolo_v5, batch_size=1)
+flops = get_flops(mobilenet_v1, batch_size=1)
 print(f"FLOPS: {flops / 10 ** 9:.03} G")
